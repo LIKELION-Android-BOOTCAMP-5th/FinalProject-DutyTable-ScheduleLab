7791ad5afe99a1d51aaf7880889c7a3e34b3467c
didgk325 <didgk6607@gmail.com>
2025-12-02 14:55:09 +0900

    add font, image, colors

---
+++ lib/core/configs/app_colors.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // --- 라이트 모드 색상 ---
+  static const Color primaryCalendarTopLight = Color(0xFFDBE9FE);
+  static const Color primaryListTopLight = Color(0xFFF3E8FF);
+  static const Color primaryChatTopLight = Color(0xFFFBE7F3);
+
+  static const Color primaryCalendartextLight = Color(0xFF3F76EE);
+  static const Color primaryListtextLight = Color(0xFF9E49ED);
+  static const Color primaryChattextLight = Color(0xFFDE357F);
+
+  static const Color bottomBarPointLight = Color(0xFFEF4444);
+  static const Color bottomBarIconDefaultLight = Color(0xFFD1D5DB);
+
+  static const Color textLight = Color(0xFF000000);
+  static const Color backgroundLight = Color(0xFFFFFFFF);
+
+  static const Color cardBackgroundLight = Color(0xFFF7F8F9);
+  static const Color cardShadowLight = Color(0xFFE7EBED);
+
+  static const Color chatMyMessageLight = Color(0xFF3C82F6);
+  static const Color chatOtherMessageLight = Color(0xFFF7F8F9);
+
+  static const Color actionPositiveLight = Color(0xFF3C82F6);
+  static const Color actionNegativeExitLight = Color(0xFFED4343);
+
+  // --- 다크 모드 색상 ---
+  static const Color primaryCalendarTopDark = Color(0xFF253B5E);
+  static const Color primaryListTopDark = Color(0xFF3B335E);
+  static const Color primaryChatTopDark = Color(0xFF4A314B);
+
+  static const Color primaryCalendartextDark = Color(0xFF5088CF);
+  static const Color primaryListtextDark = Color(0xFF9D6ED2);
+  static const Color primaryChattextDark = Color(0xFFD666A4);
+
+  static const Color bottomBarPointDark = Color(0xFFEF4444);
+  static const Color bottomBarIconDefaultDark = Color(0xFF374151);
+
+  static const Color textDark = Color(0xFFFFFFFF);
+  static const Color backgroundDark = Color(0xFF101827);
+
+  static const Color cardBackgroundDark = Color(0xFF1F2937);
+  static const Color cardShadowDark = Color(0xFF374151);
+
+  static const Color chatMyMessageDark = Color(0xFF2463EB);
+  static const Color chatOtherMessageDark = Color(0xFF303945);
+
+  static const Color actionPositiveDark = Color(0xFF3C82F6);
+  static const Color actionNegativeExitDark = Color(0xFFDB2625);
+}
