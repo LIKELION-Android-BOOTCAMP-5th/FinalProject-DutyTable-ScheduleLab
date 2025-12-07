3b1e22d922fd7ce63b7474dff91d495adcb52f5f
yjkwon <casper118412@gmail.com>
2025-12-07 20:30:15 +0900

    edit app_router.dart initialLocation

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -15,7 +15,7 @@ import 'app_shell.dart';
 
 GoRouter createRouter(BuildContext context) {
   return GoRouter(
-    initialLocation: '/calendar/edit',
+    initialLocation: '/login',
     routes: [
       // 로그인
       GoRoute(path: '/login', builder: (_, __) => const LoginScreen()),
