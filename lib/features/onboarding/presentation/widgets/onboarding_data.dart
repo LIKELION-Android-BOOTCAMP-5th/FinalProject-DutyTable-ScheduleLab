3816604a0ea085660c087089b9196f5e35976433
yw <123wlql123@gmail.com>
2025-12-19 16:22:09 +0900

    온보딩화면 이미지, 텍스트 변경

--- lib/features/onboarding/presentation/widgets/onboarding_data.dart
+++ lib/features/onboarding/presentation/widgets/onboarding_data.dart
@@ -12,18 +12,18 @@ class OnboardingData {
 
 final List<OnboardingData> onboardingPages = [
   OnboardingData(
-    title: '온보딩1',
-    body: "동아리 일정을 한눈에 관리하고\n모든 활동을 체계적으로 정리하세요",
-    image: 'assets/images/chat.png',
+    title: '일정 소통',
+    body: "선택한 캘린더 멤버들과\n일정 관련 대화를 나눠보세요",
+    image: 'assets/images/chatting.png',
   ),
   OnboardingData(
-    title: '온보딩2',
-    body: "팀원과 실시간으로 소통하고\n중요한 공지를 놓치지 마세요",
-    image: 'assets/images/calendar.png',
+    title: '일정 공유',
+    body: "가족, 친구, 팀원과 일정을 공유하고\n한눈에 확인하세요",
+    image: 'assets/images/sharing.png',
   ),
   OnboardingData(
-    title: '온보딩3',
-    body: "회비 내역을 투명하게 관리하고\n모든 거래를 한눈에 확인하세요",
-    image: 'assets/images/money.png',
+    title: '일정 알림',
+    body: "중요한 일정\n알림으로 놓치지 마세요",
+    image: 'assets/images/alarm.png',
   ),
 ];
