bc048b82ed56cfabf8c2bc08f3e2482180a28bbf
ggaeunnn <lge7022@gmail.com>
2025-12-31 14:36:41 +0900

    modify name

--- lib/features/profile/presentation/views/widgets/account_section.dart
+++ lib/features/profile/presentation/views/widgets/account_section.dart
@@ -1,5 +1,5 @@
 import 'package:dutytable/core/configs/app_colors.dart';
-import 'package:dutytable/features/profile/presentation/viewmodels/profile_viewmodel.dart';
+import 'package:dutytable/features/profile/presentation/viewmodels/profile_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
