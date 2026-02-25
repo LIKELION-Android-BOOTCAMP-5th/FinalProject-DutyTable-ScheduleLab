afd0e33a348a3d9c204ff035b2a4a2d7ea13fef2
yjkwon <casper118412@gmail.com>
2026-02-25 15:00:17 +0900

    modified: dio client - 엣지 펑션 호출을 위해 수정

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,20 +1,39 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class DioClient {
-  static final DioClient _shared = DioClient();
-  static DioClient get shared => _shared;
+  static final DioClient _instance = DioClient._internal();
+  static DioClient get shared => _instance;
 
   static const String _baseUrl = 'https://eexkppotdipyrzzjakur.supabase.co';
-
   static final String _apiKey = dotenv.env['SUPABASE_ANON_KEY']!;
 
-  final Dio _dio = Dio(
-    BaseOptions(
-      baseUrl: _baseUrl,
-      headers: {'apikey': _apiKey, 'Content-Type': 'application/json'},
-    ),
-  );
+  late final Dio _dio;
+
+  DioClient._internal() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: _baseUrl,
+        headers: {'apikey': _apiKey, 'Content-Type': 'application/json'},
+      ),
+    );
+
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          final session = Supabase.instance.client.auth.currentSession;
+          final accessToken = session?.accessToken;
+
+          if (accessToken != null) {
+            options.headers['Authorization'] = 'Bearer $accessToken';
+          }
+
+          return handler.next(options);
+        },
+      ),
+    );
+  }
 
   Dio get dio => _dio;
 }
